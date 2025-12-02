17c3826b6238ee7517815a80c650f992d556c69a
MasadRayan <masadrayan2002@gmail.com>
2025-12-02 23:49:21 +0600

    Env added

--- firebase.init.js
+++ firebase.init.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAT-mGoMNTFHFCgJrXRhPvHTksi0WyR01g",
-  authDomain: "tools-assignment-3.firebaseapp.com",
-  projectId: "tools-assignment-3",
-  storageBucket: "tools-assignment-3.firebasestorage.app",
-  messagingSenderId: "612878775322",
-  appId: "1:612878775322:web:7b559c823049ef2e9b0da3"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
